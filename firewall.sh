0d1ea87189982d7adfa67a8a0df6a1716c28abdb
jjm2473 <1129525450@qq.com>
2022-12-23 14:19:15 +0800

    support nginx proxy

--- firewall.sh
+++ firewall.sh
@@ -2,6 +2,14 @@
 LAN_IF=br-iso1
 WAN_IF=br-iso0
 
+# block VM accessing the internet
+BLOCK_PORT="22 23 101 107 512 513 992"
+BLOCK_NET="192.168.0.0/16 172.0.0.0/8 169.0.0.0/8 127.0.0.0/8 10.0.0.0/8"
+
+# forward host ports to VM's
+FORWARD_PORTS="80 443 1024:65535"
+PROXY_PORTS="80 443"
+
 patch_legacy(){
     iptables-legacy -N ISKVM_FWI && iptables-legacy -A ISKVM_FWI -j ACCEPT && iptables-legacy -A FORWARD -i $WAN_IF -j ISKVM_FWI
     iptables-legacy -N ISKVM_FWO && iptables-legacy -A ISKVM_FWO -j ACCEPT && iptables-legacy -A FORWARD -o $WAN_IF -j ISKVM_FWO
@@ -11,25 +19,40 @@ patch_legacy(){
 unblock(){
     local port
     iptables -t nat -F ISKVM_FWD
-    for port in 80 443 1024:65535 ; do
+    iptables -F ISKVM_WEB
+    # port forward
+    for port in $FORWARD_PORTS ; do
         iptables -t nat -A ISKVM_FWD -p tcp -m tcp --dport $port -j DNAT --to-destination 192.168.100.1:`echo $port | sed 's/:/-/g'`
         iptables -t nat -A ISKVM_FWD -p udp -m udp --dport $port -j DNAT --to-destination 192.168.100.1:`echo $port | sed 's/:/-/g'`
+    done
+
+    # proxy by nginx or something
+    [ -z "$PROXY_PORTS" ] && return 0
+    for port in $PROXY_PORTS ; do
+        iptables -A ISKVM_WEB -p tcp -m tcp --dport $port -j ACCEPT
+        iptables -A ISKVM_WEB -p udp -m udp --dport $port -j ACCEPT
     done
 }
 
 block(){
+    local port
     iptables -t nat -F ISKVM_FWD
+    iptables -F ISKVM_WEB
+    [ -z "$PROXY_PORTS" ] && return 0
+    for port in $PROXY_PORTS ; do
+        iptables -A ISKVM_WEB -p tcp -m tcp --dport $port -j REJECT --reject-with icmp-port-unreachable
+        iptables -A ISKVM_WEB -p udp -m udp --dport $port -j REJECT --reject-with icmp-port-unreachable
+    done
 }
 
 start(){
-    local BLOCK_NET="192.168.0.0/16 172.0.0.0/8 169.0.0.0/8 127.0.0.0/8 10.0.0.0/8"
     local chain
     local port
     local net
     stop >/dev/null 2>&1
 
     # filter table
-    for chain in ISKVM_INP ISKVM_FWI ISKVM_FWO ISKVM_FWB ISKVM_OUT ; do
+    for chain in ISKVM_INP ISKVM_FWI ISKVM_FWO ISKVM_FWB ISKVM_OUT ISKVM_WEB ; do
         iptables -N $chain
         iptables -F $chain
     done
@@ -37,7 +60,7 @@ start(){
     iptables -A ISKVM_INP -j REJECT --reject-with icmp-port-unreachable
     iptables -A ISKVM_FWI -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     # avoid being zombie
-    for port in 22 23 101 107 512 513 992 ; do
+    for port in $BLOCK_PORT ; do
         iptables -A ISKVM_FWI -p udp -m udp --dport $port -j REJECT --reject-with icmp-port-unreachable
         iptables -A ISKVM_FWI -p tcp -m tcp --dport $port -j REJECT --reject-with icmp-port-unreachable
     done
@@ -51,6 +74,7 @@ start(){
     iptables -A ISKVM_FWB -j REJECT --reject-with icmp-port-unreachable
     iptables -A ISKVM_OUT -j ACCEPT
 
+    iptables -I INPUT -j ISKVM_WEB
     iptables -I INPUT -i $WAN_IF -j ISKVM_INP
     iptables -I INPUT -i $LAN_IF -j ISKVM_INP
     iptables -I FORWARD -i $WAN_IF -j ISKVM_FWI
@@ -94,6 +118,7 @@ stop(){
     local chain
     iptables -t nat -D PREROUTING -m addrtype --dst-type LOCAL -j ISKVM_FWD
     iptables -t nat -D POSTROUTING -j ISKVM_INET
+    iptables -D INPUT -j ISKVM_WEB
     iptables -D INPUT -i $WAN_IF -j ISKVM_INP
     iptables -D INPUT -i $LAN_IF -j ISKVM_INP
     iptables -D FORWARD -i $WAN_IF -j ISKVM_FWI
@@ -103,7 +128,7 @@ stop(){
     iptables -D OUTPUT -o $LAN_IF -j ISKVM_OUT
     iptables -D OUTPUT -o $WAN_IF -j ISKVM_OUT
 
-    for chain in ISKVM_INP ISKVM_FWI ISKVM_FWO ISKVM_FWB ISKVM_OUT ; do
+    for chain in ISKVM_INP ISKVM_FWI ISKVM_FWO ISKVM_FWB ISKVM_OUT ISKVM_WEB ; do
         iptables -F $chain
         iptables -X $chain
     done
